dbe958859c57ef8abeb18b555d90491ba3ceff22
LuongLuyen <91484905+LuongLuyen@users.noreply.github.com>
2022-09-23 10:55:53 +0700

    upload axios post fix

--- website/frontend/src/Components/Upload/Upload.js
+++ website/frontend/src/Components/Upload/Upload.js
@@ -3,13 +3,26 @@ import { useState } from 'react'
 import axios from 'axios'
 import './Upload.css'
 function Upload() {
-    const url='http://localhost:5000/api/posts/add'
-    const [img,setImg]=useState()
-    const [video,setVideo]=useState()
+    const urlupload='http://localhost:5000/api/posts/upload'
+    const urladd='http://localhost:5000/api/posts/add'
+    const [upload,setUpload]=useState()
     const [title,setTitle]=useState('')
     const [content,setContent]=useState('')
+    useEffect(()=>{
+        //xóa img video củ khỏi bộ nhớ khi set img video mới
+        return ()=>{
+            //lần đầu img và video là undefined
+            upload && URL.revokeObjectURL(upload.preview)
+        }
+    },[upload])
+    const handleUpload= (e)=>{
+        const file = e.target.files[0]
+        console.log(file)
+        file.preview=URL.createObjectURL(file)
+        setUpload(file)
+    }
     const handleSubmit =()=>{
-        axios.post(url, {title,content})
+        axios.post(urladd, {title,content})
         .then((res)=>{
         console.log(res)
         })
@@ -18,26 +31,6 @@ function Upload() {
         })
         console.log({title,content})
     }
-
-    useEffect(()=>{
-        //xóa img video củ khỏi bộ nhớ khi set img video mới
-        return ()=>{
-            //lần đầu img và video là undefined
-            img && URL.revokeObjectURL(img.preview)
-            video && URL.revokeObjectURL(video.preview)
-        }
-    },[img, video])
-    const handleImg= (e)=>{
-        const file = e.target.files[0]
-        console.log(file)
-        file.preview=URL.createObjectURL(file)
-        setImg(file)
-    }
-    const handleVideo= (e)=>{
-        const file = e.target.files[0]
-        file.preview=URL.createObjectURL(file)
-        setVideo(file)
-    }         
     return ( 
         <div className='upload_wrap'>
             <form onSubmit={e=>{
@@ -46,6 +39,7 @@ function Upload() {
                 <div className='title'>
                     <h1>Thêm phim</h1>
                 </div>
+             
                 <div className='upload' >
                     <label className='upload_title'>Tên Film</label>
                     <input 
@@ -57,39 +51,31 @@ function Upload() {
                     />
                 </div>
                 <div className='upload' >
-                    <label className='upload_title'>Content</label>
+                    <label className='upload_title'>content</label>
                     <input 
                     className='upload_item'  
-                    placeholder="Nhập tên phim"
+                    placeholder="content"
                     type="text"
                     value={content}
                     onChange={e=> setContent(e.target.value)}
                     />
                 </div>
-                <div className='upload' >
-                    <label className='upload_title' >Ảnh</label>
-                    <input className='upload_item-file' onChange={handleImg} type="file"/>
-                    <div>
-                        {img && (<img src={img.preview} alt='/'/>)}
+            </form>
+            <form action={urlupload} method='POST' enctype="multipart/form-data">
+                    <div className='upload' >
+                        <label className='upload_title' >Ảnh</label>
+                        <input className='upload_item-file'name='file' onChange={handleUpload} type="file"/>
+                        <div>
+                            {upload && (<img src={upload.preview} alt=''/>)}
+                            {upload && (<video src={upload.preview} />)}
+                        </div>
                     </div>
-                </div>
-                <div className='upload' >
-                    <label className='upload_title'>Video</label>
-                    <input className='upload_item-file' onChange={handleVideo} type="file" />
-                    <div>
-                        {video && (<video className='upload_video' src={video.preview} alt='/'/>)}
-                    </div>
-                </div>
-                <div className='upload'>
-                    <label className='upload_title'>Ghi chú</label>
-                    <textarea className='upload_item'></textarea>
-                </div>
                 <div>
-                    <button onClick={handleSubmit} className='button'>Thêm phim</button>
+                    <button  onClick={handleSubmit} type='submit' className='button'>Thêm phim</button>
                 </div>
             </form>
         </div>
-     );
+    )
 }
 
 export default Upload;
