d40e828afb65bfeb48dc70050431c73ac1728865
LuongLuyen <91484905+LuongLuyen@users.noreply.github.com>
2022-10-19 11:55:10 +0700

    fix url getfilm

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   // get api
   const [data, setData] = useState(null)
   useEffect(() => {
-      axios.get(`https://luongluyen-film-server.herokuapp.com//api/posts/film`)
+      axios.get(`https://luongluyen-film-server.herokuapp.com/api/posts/film`)
       .then((response) => {
           setData(response.data)
       })
